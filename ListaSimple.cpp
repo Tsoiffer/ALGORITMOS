4f418af56783937a0b0c50df668b1007c00dc8b6
Tsoiffer <tias0708@hotmail.com>
2020-11-14 22:28:49 -0300

    Update ListaSimple.cpp

--- ListaSimple.cpp
+++ ListaSimple.cpp
@@ -1,3 +1,5 @@
+//Funciones De Listas
+
 #include <iostream>
 #include <stdio.h>
 
@@ -19,6 +21,8 @@ struct Nodo
 
 void listar(Nodo*lista);
 void insertar(Nodo*&lista, Alumno alum);
+Nodo* buscarSec(Nodo*lista,unsigned );
+Nodo* buscarSecMejorada(Nodo*lista,unsigned );
 
 int main()
 {
@@ -35,7 +39,7 @@ int main()
     }
     listar(listaK1030);
 }
-
+//Muestra los Nodos de una lista.
 void listar(Nodo*lista)
 {
     Nodo*r;
@@ -46,34 +50,34 @@ void listar(Nodo*lista)
         r=r->sig;
     }
 }
-
+//Inserta Nodo en lista.
 void insertar(Nodo*&lista, Alumno alum)
 {
     Nodo*p,*r,*ant;
     p=new Nodo;
-    p->info=alum;
+    p->info=alum;//Asigna la info que traes del programa a la info del "nuevo" nodo.
     r=lista;
-    while(r!=NULL&&r->info.leg<alum.leg)
+    while(r!=NULL&&r->info.leg<alum.leg) //Recorres la lista, la segunda condición sólo sirve si la lista está ordenada (hay que cambiarla dependiendo del problema).
     {
         ant=r;
         r=r->sig;
     }
-    p->sig=r;
+    p->sig=r; //Salís por que r es NULL o por la condición que sea, entonces P es el último por lo que se le asgina el siguiente r (que es es Null); pero si r no es Null quiere decir que P se encuentra entre r y ant, entonces el p->sig es r
     if(r!=lista)
-        ant->sig=p;
+        ant->sig=p; //si r es distinto de lista, no es el primero o sea que P es el último o está en el medio por ende se le asigna a ant->sig =p.
     else
-        lista=p;
+        lista=p; //r es igual a lista, o sea que la lista estaba vacía o sólo tenía uno.
 }
 
-Nodo* buscarSec(Nodo*lista,unsigned unLeg)
+Nodo* buscarSec(Nodo*lista,unsigned unLeg) //Buscar un nodo de forma secuencial, te devuelve la posición.
 {
     Nodo*r=lista;
-    while(r!=NULL && r->info.leg!=unLeg)
+    while(r!=NULL && r->info.leg!=unLeg) //Esta condición sirve aunque no este ordenada la lista. 
         r=r->sig;
     return r;
 }
 
-Nodo* buscarSecMejorada(Nodo*lista,unsigned unLeg)
+Nodo* buscarSecMejorada(Nodo*lista,unsigned unLeg) //Busca pero necesita que la lista tenga un orden.
 {
     Nodo*r=lista;
     while(r!=NULL && r->info.leg<unLeg)
@@ -84,24 +88,24 @@ Nodo* buscarSecMejorada(Nodo*lista,unsigned unLeg)
         return r;
 }
 
-Nodo* buscarInsertar(Nodo*&lista,Alumno al)
+Nodo* buscarInsertar(Nodo*&lista,Alumno al) //Esta función tiene dos objetivos, te devuelve si ya existe un nodo con x información o te inserta un nuevo nodo donde debe hacerlo.
 {
     Nodo*ant,*r=lista;
-    while(r!=NULL && r->info.leg<al.leg)
+    while(r!=NULL && r->info.leg<al.leg) //La condición sirve si la lista tiene un orden, pero básicamente recorre la lista.
     {
         ant=r;
         r=r->sig;
     }
 
-    if(r==NULL || r->info.leg!=unLeg) //no est� en la lista
+    if(r==NULL || r->info.leg!=al.leg) //no esta en la lista, ya sea porque la lista está vacía o si es que la lista no está vacía.
     {
         Nodo*p=new Nodo;
-        p->info=al
-        p->sig=r;
-        if(r!=lista)
+        p->info=al;
+        p->sig=r; //Si r es Null quiere decir que P es el último elemento de la lista y si r no es Null quiere decir que P está antes que r y después que ant.
+        if(r!=lista) //quiere decir que la lista no está vacía.
             ant->sig=p;
         else
-            lista=p;
+            lista=p;//la lista está vacía o tiene un sólo elemento.
         return p;
     }
     else
@@ -109,10 +113,10 @@ Nodo* buscarInsertar(Nodo*&lista,Alumno al)
 }
 
 //bool eliminar(Nodo*&lista,unsigned unLeg)
-void eliminar(Nodo*&lista,unsigned unLeg)
+void eliminar(Nodo*&lista,unsigned unLeg) //Elimina un Nodo con cierta información
 {
     Nodo*ant,*r=lista;
-    while(r!=NULL && r->info.leg<unLeg)
+    while(r!=NULL && r->info.leg<unLeg) //Recorres la lista, la segunda condición sólo sirve si está ordenada la lista.
     {
         ant=r;
         r=r->sig;
@@ -121,9 +125,9 @@ void eliminar(Nodo*&lista,unsigned unLeg)
     if(r!=NULL && r->info.leg==unLeg)//est�
     {
         if(r==lista)
-            lista=r->sig;
+            lista=r->sig; // Si el nodo que debes eliminar es el primero, directamente  a lista le asignas el siguiente de r.
         else
-            ant->sig=r->sig;
+            ant->sig=r->sig; // Si no es el primer nodo, al anterior le asingas el siguiente de r.
         delete r;
         //return true;
     }
@@ -131,7 +135,7 @@ void eliminar(Nodo*&lista,unsigned unLeg)
         //return false;
 }
 
-void mostrarAprobados(Nodo*lista)
+void mostrarAprobados(Nodo*lista) //Esta función permite mostrar nodos de la lista en particular según una condición dada.
 {
     Nodo*r;
     r=lista;
@@ -143,7 +147,7 @@ void mostrarAprobados(Nodo*lista)
     }
 }
 
-float promedioCurso(Nodo*lista)
+float promedioCurso(Nodo*lista) //Esta función muestra el uso de un contador y un sumador aplicado a listas.
 {
     unsigned cont=0,suma=0;
     Nodo*r;
@@ -159,13 +163,3 @@ float promedioCurso(Nodo*lista)
     else
         return 0;
 }
-
-
-
-
-
-
-
-
-
-
